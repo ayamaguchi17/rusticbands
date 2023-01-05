52408008a91067dd4d195768e561dc5159d40116
ayamaguchi17 <111441372+ayamaguchi17@users.noreply.github.com>
2023-01-05 12:55:12 +0900

    Close button

---
+++ script.js
@@ -0,0 +1,49 @@
+let sideNav = document.getElementById("side-nav");
+
+closeNav();
+
+function openNav() {
+    document.getElementById("side-nav").style.width = "50vw"
+    document.getElementById("blocker").style.width = "100vw"
+    sideNav.classList.add("active")
+    
+    
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0%"
+    document.getElementById("blocker").style.width = "0%"
+    sideNav.classList.remove("active")
+}
+
+
+
+
+
+//     if(sideNav.classList.contains("active")) {
+//         document.onclick = function(e) {
+//             if (e.target.id != ".btn" || e.target.id != "menu-btn"){
+//                 document.getElementById("side-nav").style.width = "0%"
+//                 sideNav.classList.remove("active")
+//             }
+//         }
+// }
+
+
+// for google map
+function initMap() {
+    // The location of uji
+    const uji = { lat: 34.89044, lng: 135.80325 };
+    // The map, centered at uji
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: uji,
+    });
+    // The marker, positioned at uji
+    const marker = new google.maps.Marker({
+        position: uji,
+        map: map,
+    });
+}
+
+window.initMap = initMap;
