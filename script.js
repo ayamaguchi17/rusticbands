c27f3f3fc3ff45a0c9cc15cefe7e5fd7a6d77ab7
ayamaguchi17 <111441372+ayamaguchi17@users.noreply.github.com>
2023-01-06 12:35:17 +0900

    JS defer

--- script.js
+++ script.js
@@ -18,20 +18,20 @@ function closeNav() {
 
 
 
-// // for google map
-// function initMap() {
-//     // The location of uji
-//     const uji = { lat: 34.89044, lng: 135.80325 };
-//     // The map, centered at uji
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 16,
-//         center: uji,
-//     });
-//     // The marker, positioned at uji
-//     const marker = new google.maps.Marker({
-//         position: uji,
-//         map: map,
-//     });
-// }
+// for google map
+function initMap() {
+    // The location of uji
+    const uji = { lat: 34.89044, lng: 135.80325 };
+    // The map, centered at uji
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: uji,
+    });
+    // The marker, positioned at uji
+    const marker = new google.maps.Marker({
+        position: uji,
+        map: map,
+    });
+}
 
-// window.initMap = initMap;
+window.initMap = initMap;
